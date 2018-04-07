012ab985dd6358eb6e293b7cb3877d0cdf4ef7c9
Nathan Stromberg <nathan.b.stromberg@gmail.com>
2018-04-07 14:29:43 -0400

    Updated scraper and output.txt

--- scrape.py
+++ scrape.py
@@ -1,7 +1,7 @@
 import codecs
 from bs4 import BeautifulSoup
-page = codecs.open("ukyallclasses.html", 'r')
-out = codecs.open("output.txt","w")
+page = codecs.open('ukyallclasses.html', 'r')
+out = codecs.open('output.txt','w')
 soup = BeautifulSoup(page.read(), 'html5lib')
 classList = soup.find(id='offering-results')
 classListAvailable=classList.find_all('div', attrs={'data-offered-in-specified-term':'True'}, recursive=False)
@@ -9,16 +9,19 @@ classListAvailable=classList.find_all('div', attrs={'data-offered-in-specified-t
 for course in classListAvailable:
     name = course.div.div.div.div.h4.get_text()
     out.write(name)
-    sectionData = [[]] #this is the problem, need to define a matrix differently
+    sectionData = [] #this is the problem, need to define a matrix differently
     i = 0
     for section in course.div.next_sibling.next_sibling.next_sibling.next_sibling.find_all('div', attrs={'class':'table-thin-row'}):
-        sectionData[i][0] = section.div.div.get_text().strip() #here is where the error becomes apparent
-        for times in section.find_all('div'):
-            part=times.find(attrs={'style':'width: 140px'})
-            sectionData[i].append(part.div.get_text()+' '+ part.div.next_sibling.next_sibling.get_text())
+        sectionData.append([])
+        sectionData[i].append(section.div.div.get_text().strip()) #here is where the error becomes apparent
+        for times in section.find_all('div',attrs={'class':'clearfix'}):
+            part=times.find('div',attrs={'style':'width: 140px;'})
+            if 'course-cancelled' not in part['class'] and 'TBD' not in part.get_text():
+                sectionData[i].append(part.div.get_text()+' '+ part.div.next_sibling.next_sibling.get_text())
         i+=1
-    out.write(' ' + len(sectionData)+'\n')
-    for i in range(len(sectionData)):
-        out.write(len(sectionData[i]+' '))
-        for j in range(len(secitonData[i])):
-            out.write(sectionData[i][j])
+    out.write(' ' + str(len(sectionData))+'\n')
+    for e in range(len(sectionData)):
+        out.write(str(len(sectionData[e]))+' ')
+        for j in range(len(sectionData[e])):
+            out.write(sectionData[e][j]+' ')
+        out.write('\n')
