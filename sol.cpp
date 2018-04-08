8d2b646b4c51276fd25c4330100a23624f33bdbe
Nelson Penn <nelsonapenn@gmail.com>
2018-04-08 11:07:19 -0400

    Added some probably incorrect implementation of makeRating and makeScaled

--- sol.cpp
+++ sol.cpp
@@ -13,6 +13,7 @@ int Sol::numDaysEx[2]={0,0};
 double Sol::compactEx[2]={0,0};
 double Sol::meanEndEx[2]={0,0};
 double Sol::meanStartEx[2]={0,0};
+int Sol::weights[4]={0,0,0,0};
 bool Sol::isOverlapping()
 {
   for(auto i=solTimes.begin();i!=solTimes.end();i++)
@@ -32,10 +33,10 @@ void Sol::makeRaw()
   }
   
   int temp=days.size();
-  if(temp>Sol::numDaysEx[1])
-    Sol::numDaysEx[1]=temp;
-  else if(temp<Sol::numDaysEx[0])
-    Sol::numDaysEx[0]=temp;
+  if(temp>numDaysEx[1])
+    numDaysEx[1]=temp;
+  else if(temp<numDaysEx[0])
+    numDaysEx[0]=temp;
   
   
   rawRatings[daysInClass]=(double)(temp);
@@ -51,6 +52,8 @@ void Sol::makeScaled()
   //Once all solutions have been found
   scaledRatings[daysInClass]=(rawRatings[daysInClass]-numDaysEx[0])/(numDaysEx[1]-numDaysEx[0]);
   scaledRatings[compactness]=(rawRatings[compactness]-compactEx[0])/(compactEx[1]-compactEx[0]);
+  double x=((double)(slider))/100.0;
+  scaledRatings[compactness]=(1-std::abs(x-scaledRatings[compactness]))/std::max(x,1-x);
   scaledRatings[lateStart]=(rawRatings[lateStart]-meanStartEx[0])/(meanStartEx[1]-meanStartEx[0]);
   scaledRatings[earlyFinish]=(meanEndEx[1]-rawRatings[earlyFinish])/(meanEndEx[1]-meanEndEx[0]);
 }
@@ -58,6 +61,11 @@ void Sol::makeScaled()
 void Sol::makeTotal()
 {
   //Once all solutions have been found
+  rating=0;
+  for(int i=0;i<weights.size();i++)
+  {
+    rating+=((double)(weights[i]))*scaledRatings[i];
+  }
 }
 
 int Sol::getRating()
@@ -67,6 +75,17 @@ int Sol::getRating()
 std::vector<Sec> Sol::getSecs()
 {
   return solSecs;
+}
+void Sol::setWeights(int[] newWeights)
+{
+  for(int i=0;i<4;i++)
+  {
+    weights[i]=newWeights[i];
+  }
+}
+void Sol::setSlider(int newSlider)
+{
+  slider=newSlider;
 }
 void Sol::addSec(Sec& newSec)
 {
--- sol.h
+++ sol.h
@@ -18,9 +18,11 @@ class Sol
     std::vector<double> rawRatings;
     std::list<STime> solTimes;
     std::vector<Sec> solSecs;
+    static int slider;
+    static int weights[4];
     static double meanStartEx[2];
     static double meanEndEx[2];
-    static int numDaysEx[2];
+    static double numDaysEx[2];
     static double compactEx[2];
   public:
     Sol();
@@ -30,6 +32,8 @@ class Sol
     void makeTotal();
     int getRating();
     std::vector<Sec> getSecs();
+    void setWeights(int[] newWeights);
+    void setSlider(int newSlider);
     void addSec(Sec& newSec);
     void print();
 
